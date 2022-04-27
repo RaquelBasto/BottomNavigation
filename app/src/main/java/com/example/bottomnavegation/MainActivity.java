3dcc8d505044dc3c7d66af2086e367c72cb43758
RaquelBasto <bastoraquel941@gmail.com>
2022-04-27 16:52:57 -0300

    Acho que agora vai

--- app/src/main/java/com/example/bottomnavegation/MainActivity.java
+++ app/src/main/java/com/example/bottomnavegation/MainActivity.java
@@ -1,17 +1,56 @@
 package com.example.bottomnavegation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-
+import android.view.LayoutInflater;
+import com.example.bottomnavegation.databinding.ActivityMainBinding;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    //private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+
+    private NavController navController;
+
+    private BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+        // binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // setContentView(binding.getRoot());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        //iniciarComponentes();
+        initNavigation();
+        initbadge();
+    }
+
+
+    private void initNavigation(){
+        //ExampleFragment fragment = (ExampleFragment) getSupportFragmentManager().findFragmentById(R.id.example_fragment);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+
+    private void initbadge(){
+        BadgeDrawable badge = bottomNavigationView.getOrCreateBadge(R.id.menu_news);
+        badge.setVisible(true);
+        badge.setNumber(60);
+
+        badge.setBackgroundColor(ContextCompat.getColor(this,R.color.black));
+        badge.setBadgeTextColor(ContextCompat.getColor(this,R.color.white));
     }
 }
